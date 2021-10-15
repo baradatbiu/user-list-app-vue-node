b672449723b5e59df6fca017cbef22990e5ef1e2
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-15 18:42:38 +0300

    Add tailwind css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/users">Users</router-link>
+  <div class="container mx-auto px-4 antialiase">
+    <div class="text-xl font-bold py-5 text-center">
+      <router-link class="transition-colors hover:text-green-500" to="/"
+        >Home
+      </router-link>
+      |
+      <router-link class="transition-colors hover:text-green-500" to="/users"
+        >Users</router-link
+      >
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style scoped>
+a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
