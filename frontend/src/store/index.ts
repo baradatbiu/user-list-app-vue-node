eca0e548db8436fa3fe5115af5ae4e1a44507865
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-21 16:29:15 +0300

    Add support store types in components

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,10 +1,18 @@
-import { createStore } from "vuex";
-import { actions } from "./actions";
+import { createStore, DispatchOptions, Store as VuexStore } from "vuex";
+import { Actions, actions } from "./actions";
 import { mutations } from "./mutations";
-import { state } from "./state";
+import { State, state } from "./state";
 
 export const store = createStore({
   state,
   mutations,
   actions,
 });
+
+export type Store = Omit<VuexStore<State>, "dispatch"> & {
+  dispatch<K extends keyof Actions>(
+    key: K,
+    payload?: Parameters<Actions[K]>[1],
+    options?: DispatchOptions
+  ): ReturnType<Actions[K]>;
+};
