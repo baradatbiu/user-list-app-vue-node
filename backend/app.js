535b1ea8c9bab101ad4c408c101da5b690c2257f
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-26 14:05:45 +0300

    Add cache manager for users api

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cacheManager from "cache-manager";
 import { fetchRandomUsers } from "./services/fetchRandomUsers.js";
+
 const app = express();
 const port = 5000;
+const memoryCache = cacheManager.caching({
+  store: "memory",
+  max: 10000,
+  ttl: 3600,
+});
 
 app.get("/api/users", async (req, res) => {
   try {
-    const response = await fetchRandomUsers();
+    const response = await memoryCache.wrap("api-users", fetchRandomUsers);
 
     res.append("Access-Control-Allow-Origin", "*");
     res.status(200).json(response);
